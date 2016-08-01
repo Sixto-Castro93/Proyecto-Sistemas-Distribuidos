40afdca24bf128e0f2bba99de69b96976f04ccb7
JordyVasquez <jgvasque@espol.edu.ec>
2016-08-01 18:36:03 -0500

    Subscribir a varios canales existentes o crear uno nuevo
    Se implemento un menu para poder subscribirse a algun canal existente o
    crear uno nuevo de ser el caso

--- RedisChat/src/RedisChat/RedisPubSub.java
+++ RedisChat/src/RedisChat/RedisPubSub.java
@@ -4,8 +4,15 @@
  * and open the template in the editor.
  */
 package RedisChat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
 /**
  *
  * @author Sixto
@@ -15,35 +22,48 @@ public class RedisPubSub {
     /**
      * @param args the command line arguments
      */
-    public static final String channel_name= "redisChannel";
-     
-    public static void main(String[] args) throws Exception
-    {      
-        JedisPool jedispool = new JedisPool("localhost");
-        final Jedis subscriberJedis = jedispool.getResource();
- 
-        final Subscriber subscriber = new Subscriber();
-        new Thread(new Runnable(){
-            public void run()
-            {
-                try
-                {
-                    System.out.println("Subscribing to " +channel_name);
-                    subscriberJedis.subscribe(subscriber,channel_name);
-                    System.out.println("Subscription ended.");
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Subscribing failed."+e);
-                }
+    public static final String channel_name = "redisChannel";
+
+    public static void main(String[] args) throws Exception {
+        final String IpServidor="localhost";
+        JedisPool jedispool = new JedisPool(IpServidor);
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("-----Bienvenidos a nuestro sistema de mensajeria-----");
+            System.out.println("Elija una de las siguientes opciones:");
+            System.out.println("1.-Subscribir/Crear Grupo");
+            System.out.println("2.-Ver Grupos Subscritos");
+            System.out.println("3.-Eliminar Grupos Subscritos");
+            System.out.println("4.-Ayuda y quienes somos");
+            System.out.println("5.-Salir");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String canalElejido = reader.readLine();
+            switch (canalElejido) {
+                case "1":
+                    Subcribir_Crear.Subcribir_Crear(IpServidor);
+                    break;
+
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    bandera = false;
+                    break;
+                default:
+                    break;
             }
-        }).start();
- 
+
+        }
+
         Jedis publisherJedis = jedispool.getResource();
-        new Publisher(publisherJedis,channel_name).start();
-        subscriber.unsubscribe();
-        jedispool.returnResource(subscriberJedis);
+        new Publisher(publisherJedis, channel_name).start();
+        //subscriber.unsubscribe();
+        //jedispool.returnResource(subscriberJedis);
         jedispool.returnResource(publisherJedis);
     }
-    
+
+   
 }
---
+++ RedisChat/src/RedisChat/Subcribir_Crear.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RedisChat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ *
+ * @author emele_000
+ */
+public class Subcribir_Crear {
+
+    public static void Subcribir_Crear(String IpServidor) throws IOException {
+
+        JedisPool jedispool = new JedisPool(IpServidor);
+        final Jedis subscriberJedis = jedispool.getResource();
+        final Subscriber subscriber = new Subscriber();
+        boolean bandera1 = true;
+        while (bandera1) {
+            System.out.println("Elija una opcion");
+            System.out.println("1.-Subscribirse a un grupo existente");
+            System.out.println("2.-Crear nuevo grupo");
+            BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+            String opcion = reader1.readLine();
+            switch (opcion) {
+                case "1":
+                    int i = 1;
+                    System.out.println("Elija el canal para subscribirse:");
+                    List<String> listaCanales = subscriberJedis.pubsubChannels("*");
+                    if (listaCanales.isEmpty()) {
+                        System.out.println("No hay canales por el momento, cree uno en su defecto");
+                        opcion = "2";
+                    } else {
+                        HashMap Base = new HashMap();
+                        for (String canal : listaCanales) {
+
+                            Base.put(Integer.toString(i), canal);
+                            System.out.println("canal_" + i + ": " + canal);
+                            i++;
+                        }
+                        String opcion_canal = reader1.readLine();
+                        final String cn = (String) Base.get(opcion_canal);
+                        if (cn != null) {
+
+                            System.out.println("Ingrese un nombre para identificarse en este grupo");
+                            String name = reader1.readLine();
+                            if (name != null) {
+
+                                bandera1 = false;
+                                System.out.println("Subscrito a  " + cn + " con nombre :" + name);
+                                new Thread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        try {
+
+                                            subscriberJedis.subscribe(subscriber, (String) cn);
+                                            System.out.println("Subscricion terminada a: " + cn);
+
+                                        } catch (Exception e) {
+                                            System.out.println("Subscribing failed." + e);
+                                        }
+                                    }
+                                }).start();
+                            }
+                        } else {
+                            System.out.println("Ese canal no esta dentro del rango de opciones");
+                        }
+                        break;
+                    }
+
+                case "2":
+                    System.out.println("Ingrese el nombre del canal:");
+                    final String canal_new = reader1.readLine();
+                    if (canal_new != null) {
+                        System.out.println("Ingrese un nombre para identificarse en este grupo");
+                        String name = reader1.readLine();
+                        if (name != null) {
+
+                            bandera1 = false;
+                            System.out.println("Subscrito a  " + canal_new + " con nombre :" + name);
+
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+
+                                        subscriberJedis.subscribe(subscriber, (String) canal_new);
+                                        System.out.println("Subscricion terminada a: " + canal_new);
+
+                                    } catch (Exception e) {
+                                        System.out.println("Subscribing failed." + e);
+                                    }
+                                }
+                            }).start();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
